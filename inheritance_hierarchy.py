2c773e283693235fa6b77e39e87e97f5c0e196c2
benzics <benjaminnicholas29@gmail.com>
2022-08-11 10:59:20 +0100

    Fixed indentation mistake in account class

--- inheritance_hierarchy.py
+++ inheritance_hierarchy.py
@@ -6,22 +6,22 @@ class Account:
         else:
             self.balance = float(balance)
 
-        #credit method to add to the account balance
-        def credit(self, amount):
-            self.balance += float(amount)
+    #credit method to add to the account balance
+    def credit(self, amount):
+        self.balance += float(amount)
 
-        #debit method that debits from the account balance
-        def debit(self, amount):
-            if self.balance < amount:
-                print("Insufficient funds")
-                return False
-            else:
-                self.balance -= float(amount)
-                return True
+    #debit method that debits from the account balance
+    def debit(self, amount):
+        if self.balance < amount:
+            print("Insufficient funds")
+            return False
+        else:
+            self.balance -= float(amount)
+            return True
 
-        #returns the current balance
-        def get_balance(self):
-            return self.balance
+    #returns the current balance
+    def get_balance(self):
+        return self.balance
 
 
 class Savings_Account(Account):
@@ -47,3 +47,17 @@ class Checking_Account(Account):
     def debit(self, amount):
         if super().debit(amount):
             self.balance -= self.fee
+
+#tests for the classes here
+
+#test for Account class with less than 0 balance
+test_account = Account(0)
+
+# credit the account
+test_account.credit(1000)
+
+# debit the account
+test_account.debit(97.5)
+
+# print account balance
+print(test_account.get_balance())
